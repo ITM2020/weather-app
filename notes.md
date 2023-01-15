927a45aebaa3024325b4a764acb380854065318a
Nathan Hoang <nathanhoang123@gmail.com>
2023-01-15 11:07:05 -0500

    Remove default create-react-app files

---
+++ notes.md
@@ -0,0 +1,3 @@
+display saved locations on map with pins/markers with weather as the icon
+
+Current weather, hourly 4 day forecast, 3hr 5 day forecast, daily 16 day forecast
--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [searchResults, setSearchResults] = useState()
+  
+  const handleChange = async (event) => {
+    const response = await axios.get(`${process.env.GEOAPIFY_API_URL}`+`text=${event.target.value}`+``)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" />
     </div>
-  );
+  )
 }
-
-export default App;
